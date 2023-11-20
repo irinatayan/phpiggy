7e768b38f81a7ae6476e270526a56456b1288802
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-11-20 12:30:16 +0200

    add dispatcher to router

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -8,13 +8,14 @@ class Router
 {
     private array $routes = [];
 
-    public function add(string $method, string $path): void
+    public function add(string $method, string $path, array $controller): void
     {
         $path = $this->normalizePath($path);
 
         $this->routes[] = [
             'path' => $path,
-            'method' => strtoupper($method)
+            'method' => strtoupper($method),
+            'controller' => $controller
         ];
     }
 
@@ -26,4 +27,21 @@ class Router
 
         return $path;
     }
+
+    public function dispatch(string $path, string $method): void
+    {
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        foreach ($this->routes as $route) {
+            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method) {
+                continue;
+            }
+            [$class, $function] = $route['controller'];
+            $controllerInstance = new $class;
+            $controllerInstance->{$function}();
+        }
+    }
+
+
 }
