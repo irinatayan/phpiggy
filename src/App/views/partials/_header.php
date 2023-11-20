273782d8362b2a297840b00188fb6c7d3eaef2d2
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-11-20 17:26:32 +0200

    escaping function

--- src/App/views/partials/_header.php
+++ src/App/views/partials/_header.php
@@ -1,10 +1,13 @@
+<?php
+/** @var string $title */
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>PHPiggy</title>
+    <title>PHPiggy - <?= e($title) ?></title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
