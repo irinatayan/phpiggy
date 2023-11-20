c3bf89c3c03f7f5f044fe51e21944d3c9ccf78c7
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-11-20 11:46:11 +0200

    router

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -5,9 +5,20 @@ namespace Framework;
 
 class App
 {
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
     public function run(): void
     {
         echo "Application is running.";
     }
 
+    public function get(string $path): void
+    {
+        $this->router->add('GET', $path);
+    }
 }
