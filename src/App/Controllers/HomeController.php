3c5dcfb58de9f47b79e88716fc7a3e9672224e58
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-11-21 19:13:50 +0200

    dependency injection container - done

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -8,11 +8,10 @@ use Framework\TemplateEngine;
 use App\Config\Paths;
 class HomeController
 {
-    private TemplateEngine $view;
 
-    public function __construct()
+    public function __construct(private TemplateEngine $view)
     {
-        $this->view = new TemplateEngine(Paths::VIEW);
+
     }
 
     public function home(): void
---
+++ src/Framework/Exceptions/ContainerException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework\Exceptions;
+
+class ContainerException extends \Exception
+{
+
+}
