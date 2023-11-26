24b6e7a53c3b49bde891521b26457b62927da4c8
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-11-26 17:27:19 +0200

    prefilling a registration form

--- src/App/Middleware/FlashMiddleware.php
+++ src/App/Middleware/FlashMiddleware.php
@@ -17,6 +17,8 @@ class FlashMiddleware implements MiddlewareInterface
     {
         $this->view->addGlobal('errors', $_SESSION['errors'] ?? []);
         unset($_SESSION['errors']);
+        $this->view->addGlobal('oldFormData', $_SESSION['oldFormData'] ?? []);
+        unset($_SESSION['oldFormData']);
         $next();
     }
 }
--- src/App/views/register.php
+++ src/App/views/register.php
@@ -1,8 +1,9 @@
 <?php include $this->resolve("partials/_header.php"); ?>
 <?php /** @var array $errors */ ?>
+<?php /** @var array $oldFormData */ ?>
 
 <?php
-$ss = "d";
+$rr = 5;
 ?>
 <section
     class="max-w-2xl mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded"
@@ -12,6 +13,7 @@ $ss = "d";
         <label class="block">
             <span class="text-gray-700">Email address</span>
             <input
+                value="<?=e($oldFormData['email'] ?? ''); ?>"
                 type="email"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="john@example.com"
@@ -25,6 +27,7 @@ $ss = "d";
         <label class="block">
             <span class="text-gray-700">Age</span>
             <input
+                value="<?=e($oldFormData['age'] ?? ''); ?>"
                 type="number"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder=""
@@ -41,8 +44,8 @@ $ss = "d";
                 class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
             >
                 <option value="USA">USA</option>
-                <option value="Canada">Canada</option>
-                <option value="Mexico">Mexico</option>
+                <option value="Canada" <?php echo isset($oldFormData['country']) && $oldFormData['country'] === 'Canada' ? 'selected' : ''; ?>>Canada</option>
+                <option value="Mexico" <?php echo isset($oldFormData['country'])  && $oldFormData['country'] === 'Mexico' ? 'selected' : ''; ?>>Mexico</option>
                 <option value="Invalid">Invalid Country</option>
             </select>
             <?php if (array_key_exists('country', $errors)): ?>
@@ -53,6 +56,7 @@ $ss = "d";
         <label class="block">
             <span class="text-gray-700">Social Media URL</span>
             <input
+                value="<?php echo e($oldFormData['socialMediaURL'] ?? ''); ?>"
                 type="text"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="socialMediaURL"
@@ -94,6 +98,7 @@ $ss = "d";
                 <div>
                     <label class="inline-flex items-center">
                         <input
+                            <?php echo $oldFormData['tos'] ?? false ? 'checked' : ''; ?>
                             class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
                             type="checkbox" name="tos"
                         />
