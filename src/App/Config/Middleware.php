305a815a3887ceeabeba4441d373a8d30a0af123
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-11-26 12:26:27 +0200

    sessions

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Middleware\{TemplateDataMiddleware, ValidationExceptionMiddleware};
+use App\Middleware\{SessionMiddleware, TemplateDataMiddleware, ValidationExceptionMiddleware};
 
 function registerMiddleware(App $app)
 {
     $app->addMiddleware(TemplateDataMiddleware::class );
     $app->addMiddleware(ValidationExceptionMiddleware::class );
+    $app->addMiddleware(SessionMiddleware::class );
 }
