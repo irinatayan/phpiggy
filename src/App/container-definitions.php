abd19fecfccaa82cc44302ba7a085b78994451dd
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-11-28 11:35:53 +0200

    add database class to container

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
-use Framework\TemplateEngine;
+use Framework\{TemplateEngine, Database};
 use App\Services\ValidatorService;
 use App\Config\Paths;
 
 return [
     TemplateEngine::class => fn() => new TemplateEngine(Paths::VIEW),
     ValidatorService::class => fn() => new ValidatorService(),
-
+    Database::class => fn() => new Database(
+        'mysql',
+        [
+            'host' => '127.0.0.1',
+            'port' => 3307,
+            'dbname' => 'phpiggy'
+        ],
+        'root',
+        'electioneering'
+    )
 ];
