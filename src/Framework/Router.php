f470b22b2e49e41810bbf13e50f8faf463ae29a5
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-11-30 18:59:56 +0200

    fix router

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Framework\Exceptions\ContainerException;
+
 class Router
 {
     private array $routes = [];
-    private array $middleware = [];
-
+    private array $middlewares = [];
 
     public function add(string $method, string $path, array $controller): void
     {
@@ -24,40 +25,51 @@ class Router
     private function normalizePath(string $path): string
     {
         $path = trim($path, '/');
-        $path = "/$path/";
-        return preg_replace("#/{2,}#", '/', $path);
+        $path = "/{$path}/";
+        $path = preg_replace('#[/]{2,}#', '/', $path);
+
+        return $path;
     }
 
+    /**
+     * @throws ContainerException
+     */
     public function dispatch(string $path, string $method, Container $container = null): void
     {
         $path = $this->normalizePath($path);
         $method = strtoupper($method);
 
         foreach ($this->routes as $route) {
-            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method) {
+            if (
+                !preg_match("#^{$route['path']}$#", $path) ||
+                $route['method'] !== $method
+            ) {
                 continue;
             }
+
             [$class, $function] = $route['controller'];
+
             $controllerInstance = $container ?
                 $container->resolve($class) :
                 new $class();
 
             $action = fn() => $controllerInstance->{$function}();
 
-            foreach ($this->middleware as $middleware) {
-                $middlewareInstance = $container ? $container->resolve($middleware) : new $middleware();
+            foreach ($this->middlewares as $middleware) {
+                $middlewareInstance = $container ?
+                    $container->resolve($middleware) :
+                    new $middleware();
                 $action = fn() => $middlewareInstance->process($action);
             }
 
             $action();
+
             return;
         }
     }
 
-    public function addMiddleware(string $middleware)
+    public function addMiddleware(string $middleware): void
     {
-        $this->middleware[] = $middleware;
+        $this->middlewares[] = $middleware;
     }
-
-
 }
