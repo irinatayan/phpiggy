2ff5963f6e03ec55ab82c9a05388dfd1448440e8
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-12-01 14:13:34 +0200

    create transaction page

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Controllers\{HomeController, AboutController, AuthController};
+use App\Controllers\{HomeController, AboutController, AuthController, TransactionController};
 use App\Middleware\{AuthRequiredMiddleware, GuestOnlyMiddleware};
 
 function registerRoutes(App $app): void
@@ -17,4 +17,5 @@ function registerRoutes(App $app): void
     $app->get('/login', [AuthController::class, 'loginView'])->add(GuestOnlyMiddleware::class);
     $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
     $app->get('/logout', [AuthController::class, 'logout'])->add(AuthRequiredMiddleware::class);
+    $app->get('/transaction', [TransactionController::class, 'create'])->add(AuthRequiredMiddleware::class);
 }
