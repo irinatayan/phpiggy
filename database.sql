f4d90f8975867b3a30c26545e10d6eb4b21ca3a5
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-12-01 14:05:22 +0200

    add transaction table

--- database.sql
+++ database.sql
@@ -1,14 +1,27 @@
-CREATE TABLE IF NOT EXISTS users
+create table if not exists users
 (
-    id               bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    email            varchar(255)        NOT NULL,
-    password         varchar(255)        NOT NULL,
-    age              tinyint(3) unsigned NOT NULL,
-    country          varchar(255)        NOT NULL,
-    social_media_url varchar(255)        NOT NULL,
-    created_at       datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at       datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    PRIMARY KEY (id),
-    UNIQUE KEY (email)
+    id               bigint(20) unsigned not null auto_increment,
+    email            varchar(255)        not null,
+    password         varchar(255)        not null,
+    age              tinyint(3) unsigned not null,
+    country          varchar(255)        not null,
+    social_media_url varchar(255)        not null,
+    created_at       datetime            not null default current_timestamp(),
+    updated_at       datetime            not null default current_timestamp(),
+    primary key (id),
+    unique key (email)
 );
 
+
+create table if not exists transactions
+(
+    id          bigint(20)          not null auto_increment,
+    description varchar(255)        not null,
+    amount      decimal(10, 2)      not null,
+    date        datetime            not null,
+    created_at  datetime            not null default current_timestamp(),
+    updated_at  datetime            not null default current_timestamp(),
+    user_id     bigint(20) unsigned not null,
+    primary key (id),
+    foreign key (user_id) references users (id)
+);
