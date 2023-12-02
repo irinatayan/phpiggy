0dfd398e9b6cb9e612a4700220d302f3a630a300
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-12-02 13:53:02 +0200

    creating a transaction

---
+++ src/App/Services/TransactionService.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Framework\Database;
+
+class TransactionService
+{
+    public function __construct(private Database $db)
+    {
+    }
+
+    public function create(array $formData): void
+    {
+        $formattedDate = "{$formData['date']} 00:00:00";
+
+        $this->db->query("insert into transactions(user_id, description, amount, date) 
+                                values (:user_id, :description, :amount, :date)",
+        [
+            'user_id' => $_SESSION['user'],
+            'description' => $formData['description'],
+            'amount' => $formData['amount'],
+            'date' => $formattedDate,
+        ]);
+
+
+
+    }
+}
